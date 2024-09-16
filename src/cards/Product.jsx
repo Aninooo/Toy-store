87a735a35af595d2e02aaf12313e2777d71f3d97
Ani no <bryanlomerioanino@gmail.com>
2024-09-16 20:56:12 +0800

    improved the products

--- src/cards/Product.jsx
+++ src/cards/Product.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Product.css';
+import { FaRegHeart } from "react-icons/fa";
+import { FaLink } from 'react-icons/fa';
 
 const Product = ({ image, title, description, price }) => {
   const formattedPrice = new Intl.NumberFormat('en-PH', {
@@ -11,9 +13,29 @@ const Product = ({ image, title, description, price }) => {
     alert(`${title} added to cart!`);
   };
 
+  const handleWishlist = () => {
+    alert(`${title} added to wishlist!`);
+  };
+
+  const handleCopyLink = () => {
+    navigator.clipboard.writeText(window.location.href);
+    alert('Link copied to clipboard!');
+  };
+
   return (
     <div className="product-card">
       <img src={image} alt={title} className="product-image" />
+      <div className="product-footer">
+        <button className="icon-button" onClick={handleWishlist}>
+        <FaRegHeart />
+        </button>
+        <button className="icon-button" onClick={handleCopyLink}>
+          <FaLink />
+        </button>
+        <button className="add-to-cart-button" onClick={handleAddToCart}>
+          Add to Cart
+        </button>
+      </div>
       <div className="product-info">
         <div>
           <h2 className="product-title">{title}</h2>
@@ -21,9 +43,6 @@ const Product = ({ image, title, description, price }) => {
         </div>
         <div>
           <span className="product-price">{formattedPrice}</span>
-          <button className="add-to-cart-button" onClick={handleAddToCart}>
-            Add to Cart
-          </button>
         </div>
       </div>
     </div>
