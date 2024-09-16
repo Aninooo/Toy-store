32911825c6fab92780e38c8a930295d1a1bc45c7
Ani no <bryanlomerioanino@gmail.com>
2024-09-16 12:10:42 +0800

    added search button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,45 @@
-
-import React from 'react'
-import './header.css'
+import React, { useState } from 'react';
+import './header.css';
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoMdCart } from "react-icons/io";
+import { Link, useNavigate } from 'react-router-dom';
+
 function Header() {
+  const [query, setQuery] = useState('');
+  const navigate = useNavigate();
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    if (query) {
+      navigate(`/search?q=${query}`);
+    }
+  };
+
   return (
     <header>
-    <div className='hamburger-menu'><GiHamburgerMenu /></div>
-    <div>
-    
-    <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/account">Acount</a></li>
-        <li><a href="/search">Search</a></li>
-        <li><a href="/cart" className='cart-icon'><IoMdCart/></a></li>
-
-    </ul>
-    </div>
+      <div className='hamburger-menu'><GiHamburgerMenu /></div>
+      <h1 className='title'>Anino's Toy Collections</h1>
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+          <li><Link to="/account">Account</Link></li>
+          <form onSubmit={handleSearch} className='search-form'>
+        <input
+          type="text"
+          placeholder='Search Toys...'
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+          <li><Link to="/cart" className='cart-icon'><IoMdCart /></Link></li>
+          
+        </ul>
+      </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import Contact from './pages/Contact.jsx';
+import Account from './pages/Account.jsx';
+import Search from './pages/Search.jsx';
+import Header from './components/Header.jsx'; // Import Header component
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function App() {
+  return (
+    <BrowserRouter>
+      <Header /> {/* Include Header here */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="account" element={<Account />} />
+        <Route path="search" element={<Search />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
