f70a8ac7f610efbce8d6fe1e09f95868afc71f57
Ani no <bryanlomerioanino@gmail.com>
2024-09-18 10:26:16 +0800

    added socials

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import { IoIosCall } from "react-icons/io";
+import { SiFacebook } from "react-icons/si";
+import { FaQuestionCircle } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 import './Home.css'; 
 import Law from '../assets/Law.png';
 import Corazon from '../assets/img1.jpg';
@@ -164,17 +168,59 @@ const Home = () => {
       </div>
     </div>
 
-<footer>
-  <div className='footer-info'>
-    <h4><b>Information</b></h4>
-    <a href="">Search</a>
-    <a href="">FAQs</a>
-    <a href="">Payment Methods</a>
-    <a href="">Shipping/Pick-up Details</a>
-    <a href="">About us</a>
+    <footer>
+  <div className='footer-main'>
+    <div className='footer-info'>
+      <h4><b>Information</b></h4>
+      <a href="#">Search</a>
+      <a href="#">FAQs</a>
+      <a href="#">Payment Methods</a>
+      <a href="#">Shipping/Pick-up Details</a>
+      <a href="#">About us</a>
+    </div>
+    <div className='footer-account'>
+      <h4><b>My Account</b></h4>
+      <a href="#">Login</a>
+      <a href="#">Register</a>
+      <a href="#">Profile</a>
+    </div>
+    <div className='get-in-touch'>
+    <h4><b>Get in touch</b></h4>
+      <a href="#">Location & Hours</a>
+      <a href="#">bryanlomerioanino@gmail.com</a>
+    </div>
+    <div className='needsomehelp'>
+    <h2>Need some help?</h2>
+    <p>We're here and ready by phone and email every day,</p>
+    <p>M-Sat 10am-6pm</p>
+    <div className="need-help-icons">
+    <div className="icon-label">
+        <a className='call' href=""><IoIosCall /></a>
+        <label htmlFor="call">Call</label>
+    </div>
+    <div className="icon-label">
+        <a className='facebook' href=""><SiFacebook /></a>
+        <label htmlFor="facebook">Facebook page</label>
+    </div>
+    <div className="icon-label">
+        <a className='faq' href=""><FaQuestionCircle /></a>
+        <label htmlFor="faqs">FAQ</label>
+    </div>
+    <div className="icon-label">
+        <a className='email' href=""><MdEmail /></a>
+        <label htmlFor="email">Email</label>
+    </div>
+</div>
+
+  </div>
+  </div>
+  <div className='copyrights'>
+    <p>&copy; {new Date().getFullYear()} Anino's Toy Collections. All Rights Reserved.</p>
   </div>
 </footer>
+
     </div>
+    
   );
 };
 
