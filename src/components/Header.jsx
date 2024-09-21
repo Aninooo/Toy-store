5748fd1a905622a35ffa84814e4cacd8e7672ebc
Ani no <bryanlomerioanino@gmail.com>
2024-09-21 11:21:43 +0800

    fixed the media queries

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,11 @@ import { VscAccount } from "react-icons/vsc";
 import { IoMdArrowDropdown } from "react-icons/io";
 import Logo from '../assets/logo-Law.png';
 import { GiHamburgerMenu } from "react-icons/gi";
-import Trust from '../assets/trust_badge.avif'
+import Trust from '../assets/trust_badge.avif';
 
 function Header() {
   const [query, setQuery] = useState('');
+  const [isNavOpen, setIsNavOpen] = useState(false);
   const [isProductDropdownOpen, setIsProductDropdownOpen] = useState(false);
   const [isBrandsDropdownOpen, setIsBrandsDropdownOpen] = useState(false);
   const [isSearchOpen, setIsSearchOpen] = useState(false);
@@ -22,7 +23,7 @@ function Header() {
     event.preventDefault();
     if (query) {
       navigate(`/search?q=${query}`);
-      setIsSearchOpen(false); 
+      setIsSearchOpen(false);
     }
   };
 
@@ -36,6 +37,10 @@ function Header() {
 
   const toggleSearch = () => {
     setIsSearchOpen(!isSearchOpen);
+  };
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
   };
 
   useEffect(() => {
@@ -56,74 +61,75 @@ function Header() {
 
   return (
     <>
-      <div className='head-head'><a className='fbpage' href="https://www.facebook.com/AninoGaming"><span className='underline'>Follow Anino's Toy Collections Facebook page by Clicking here</span></a></div>
-   
-    <header>
-      <div className='logo-container'>
-        <img className='logo' src={Logo} alt="Logo" />
+      <div className='head-head'>
+        <a className='fbpage' href="https://www.facebook.com/AninoGaming">
+          <span className='underline'>Follow Anino's Toy Collections Facebook page by Clicking here</span>
+        </a>
       </div>
-      <div className='nav-container'>
-        <ul className="nav-links">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/newarrivals">New Arrivals</Link></li>
-          <li className="dropdown">
-            <button 
-              className={`dropdown-toggle ${isProductDropdownOpen ? 'active' : ''}`} 
-              onClick={toggleProductDropdown}
-            >
-              All Products <IoMdArrowDropdown className="dropdown-icon" />
-            </button>
-            <ul className={`dropdown-menu ${isProductDropdownOpen ? 'show' : ''}`} >
-              <li><Link to="/big-sale">Big Sale</Link></li>
-              <li><Link to="/on-stack">On Stack</Link></li>
-            </ul>
-          </li>
-          <li className="dropdown">
-            <button 
-              className={`dropdown-toggle ${isBrandsDropdownOpen ? 'active' : ''}`} 
-              onClick={toggleBrandsDropdown}
-            >
-              Brands / Toylines <IoMdArrowDropdown className="dropdown-icon" />
-            </button>
-            <ul className={`dropdown-menu ${isBrandsDropdownOpen ? 'show' : ''}`} >
-              <li><Link to="/brands/bandai">Bandai</Link></li>
-              <li><Link to="/brands/banpresto">Banpresto</Link></li>
-              <li><Link to="/brands/whos-studio">WHO/S Studio</Link></li>
-              <li><Link to="/brands/fox-studios">Fox Studios</Link></li>
-              <li><Link to="/brands/x6-studio">X6 Studio</Link></li>
-            </ul>
-          </li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-        </ul>
-       
-        <div className="icon-links">
-          <img className='trust-img' src={Trust} alt="" />
-        <div className='hamburger-menu'><GiHamburgerMenu /></div>
-          <div ref={searchButtonRef} style={{ display: 'inline-block' }}>
-            <CiSearch 
-              onClick={toggleSearch} 
-              style={{ cursor: 'pointer' }} 
-            />
+      <header>
+        <div className='nav-container'>
+          <div className='logo-container'>
+            <img className='logo' src={Logo} alt="Logo" />
           </div>
-          <Link to="/account">{<VscAccount style={{ fontSize: '20px' }} />}</Link>
-          <Link to="/cart"><CiShoppingCart /></Link>
+          <div className='hamburger-menu' onClick={toggleNav}>
+            <GiHamburgerMenu />
+          </div>
+          <ul className={`nav-links ${isNavOpen ? 'show' : ''}`}>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/newarrivals">New Arrivals</Link></li>
+            <li className="dropdown">
+              <button 
+                className={`dropdown-toggle ${isProductDropdownOpen ? 'active' : ''}`} 
+                onClick={toggleProductDropdown}
+              >
+                All Products <IoMdArrowDropdown className="dropdown-icon" />
+              </button>
+              <ul className={`dropdown-menu ${isProductDropdownOpen ? 'show' : ''}`}>
+                <li><Link to="/big-sale">Big Sale</Link></li>
+                <li><Link to="/on-stack">On Stack</Link></li>
+              </ul>
+            </li>
+            <li className="dropdown">
+              <button 
+                className={`dropdown-toggle ${isBrandsDropdownOpen ? 'active' : ''}`} 
+                onClick={toggleBrandsDropdown}
+              >
+                Brands / Toylines <IoMdArrowDropdown className="dropdown-icon" />
+              </button>
+              <ul className={`dropdown-menu ${isBrandsDropdownOpen ? 'show' : ''}`}>
+                <li><Link to="/brands/bandai">Bandai</Link></li>
+                <li><Link to="/brands/banpresto">Banpresto</Link></li>
+                <li><Link to="/brands/whos-studio">WHO/S Studio</Link></li>
+                <li><Link to="/brands/fox-studios">Fox Studios</Link></li>
+                <li><Link to="/brands/x6-studio">X6 Studio</Link></li>
+              </ul>
+            </li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
+          <div className="icon-links">
+            <img className='trust-img' src={Trust} alt="" />
+            <div ref={searchButtonRef} style={{ display: 'inline-block' }}>
+              <CiSearch onClick={toggleSearch} style={{ cursor: 'pointer' }} />
+            </div>
+            <Link to="/account">{<VscAccount style={{ fontSize: '20px' }} />}</Link>
+            <Link to="/cart"><CiShoppingCart /></Link>
+          </div>
         </div>
-      </div>
-      {isSearchOpen && (
-        <div ref={searchPopupRef} className="search-popup">
-          <form onSubmit={handleSearch} className="search-form">
-            <input 
-              type="text" 
-              placeholder="Search..." 
-              value={query} 
-              onChange={(e) => setQuery(e.target.value)} 
-            />
-            <button type="submit">Search</button>
-          </form>
-        </div>
-      )}
-    </header>
+        {isSearchOpen && (
+          <div ref={searchPopupRef} className="search-popup">
+            <form onSubmit={handleSearch} className="search-form">
+              <input 
+                type="text" 
+                placeholder="Search..." 
+                value={query} 
+                onChange={(e) => setQuery(e.target.value)} 
+              />
+              <button type="submit">Search</button>
+            </form>
+          </div>
+        )}
+      </header>
     </>
   );
 }
