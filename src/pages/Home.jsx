5874a553faeb07b5eae9b0c1a131fcb5be1f571e
anino secret <bryanlomerioanino@gmail.com>
2024-10-24 16:21:15 +0800

    added view more btn

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { gifRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -10,36 +10,42 @@ import KidLaw from '../assets/img2.jpg';
 import Beepo from '../assets/img3.jpg';
 
 //For slider
-import Windbreaker from '../assets/slider/windbreaker.png'
-import TowerOfGod from '../assets/slider/towerofgod.png'
-import Bleach from '../assets/slider/bleach.png'
-import DemonSlayer from '../assets/slider/demonslayer.png'
-import Dragonball from '../assets/slider/dragonball.png'
-import HeroAcademia from '../assets/slider/heroacademia.png'
-import Onepiece from '../assets/slider/onepiece.png'
-import Slamdunk from '../assets/slider/slamdunk.png'
-import Transformers from '../assets/slider/transformers.png'
-import Pokemon from '../assets/slider/pokemon.png'
+import Windbreaker from '../assets/slider/windbreaker.png';
+import TowerOfGod from '../assets/slider/towerofgod.png';
+import Bleach from '../assets/slider/bleach.png';
+import DemonSlayer from '../assets/slider/demonslayer.png';
+import Dragonball from '../assets/slider/dragonball.png';
+import HeroAcademia from '../assets/slider/heroacademia.png';
+import Onepiece from '../assets/slider/onepiece.png';
+import Slamdunk from '../assets/slider/slamdunk.png';
+import Transformers from '../assets/slider/transformers.png';
+import Pokemon from '../assets/slider/pokemon.png';
 import Product from '../cards/Product';
 
-import CorazonProducts from '../assets/products/corazon.png'
-import LawMDProducts from '../assets/products/lawMD.png'
-import DoflamingoProducts from '../assets/products/doflamingo.png'
-import PopDoffyProducts from '../assets/products/popdoffy.png'
-import ResinCorazonProducts from '../assets/products/resincorazon.png'
-import DoffyCoraProducts from '../assets/products/doffycora.png'
-import G5LuffyProducts from '../assets/products/G5Luffy.png'
-import ZoroProducts from '../assets/products/zoro.png'
-import PokemonGirl from '../assets/products/pokemon-girl.png'
+import CorazonProducts from '../assets/products/corazon.png';
+import LawMDProducts from '../assets/products/lawMD.png';
+import DoflamingoProducts from '../assets/products/doflamingo.png';
+import PopDoffyProducts from '../assets/products/popdoffy.png';
+import ResinCorazonProducts from '../assets/products/resincorazon.png';
+import DoffyCoraProducts from '../assets/products/doffycora.png';
+import G5LuffyProducts from '../assets/products/G5Luffy.png';
+import ZoroProducts from '../assets/products/zoro.png';
+import PokemonGirl from '../assets/products/pokemon-girl.png';
 
-import Banpresto from '../assets/brands/banpresto.png'
-import Megahouse from '../assets/brands/megahouse.png'
-import Kotobukiya from '../assets/brands/Kotobukiya.png'
-import Aoshima from '../assets/brands/Aoshima.png'
-import Bandai from '../assets/brands/bandai.png'
-import IchibanKuji from '../assets/brands/ichiban.png'
-import Animes from '../assets/animes.jpg'
+import LuffyKid from '../assets/products/luffykid.png';
+import LuffyG5 from '../assets/products/luffyg5.png';
+import LuffyYehey from '../assets/products/luffyyehey.png';
+import Six from '../assets/products/six.png';
+import LuffyCap from '../assets/products/luffycap.png';
 
+
+import Banpresto from '../assets/brands/banpresto.png';
+import Megahouse from '../assets/brands/megahouse.png';
+import Kotobukiya from '../assets/brands/Kotobukiya.png';
+import Aoshima from '../assets/brands/Aoshima.png';
+import Bandai from '../assets/brands/bandai.png';
+import IchibanKuji from '../assets/brands/ichiban.png';
+import Animes from '../assets/animes.jpg';
 
 const images = [Corazon, KidLaw, Beepo];
 const animeLogos = [Bleach, DemonSlayer, Dragonball, Windbreaker, HeroAcademia, TowerOfGod, Onepiece, Slamdunk, Transformers, Pokemon];
@@ -93,8 +99,8 @@ const Home = () => {
     ]
   };
 
-  const [visibleProducts, setVisibleProducts] = useState(9);
-  const products = [
+  const [visibleProducts, setVisibleProducts] = useState(12);
+  const [products, setProducts] = useState([
     {
       id: 1,
       image: CorazonProducts,
@@ -127,7 +133,7 @@ const Home = () => {
       id: 5,
       image: ResinCorazonProducts,
       title: 'Donquixote Rosinante Resin statue',
-      description: 'A brand-new, unused, unopened, undamaged item..',
+      description: 'A brand-new, unused, unopened, undamaged item.',
       price: 32984
     },
     {
@@ -148,7 +154,7 @@ const Home = () => {
       id: 8,
       image: ZoroProducts,
       title: '1/6 Scale Egghead Island Arc Roronoa Zoro',
-      description: ' ONE PIECE Resin Statue - X6 Studio [Pre-Order]',
+      description: 'ONE PIECE Resin Statue - X6 Studio [Pre-Order]',
       price: 7616
     },
     {
@@ -157,12 +163,50 @@ const Home = () => {
       title: 'May with Torchic',
       description: 'Pokemon Kotobukiya May with Torchic Figurine',
       price: 2900
+    },
+    {
+      id: 10,
+      image: LuffyKid,
+      title: 'May with Torchic',
+      description: 'Pokemon Kotobukiya May with Torchic Figurine',
+      price: 2900
+    },
+    {
+      id: 11,
+      image: LuffyG5,
+      title: 'May with Torchic',
+      description: 'Pokemon Kotobukiya May with Torchic Figurine',
+      price: 2900
+    },
+    {
+      id: 12,
+      image: LuffyYehey,
+      title: 'May with Torchic',
+      description: 'Pokemon Kotobukiya May with Torchic Figurine',
+      price: 2900
+    },
+    {
+      id: 13,
+      image: LuffyCap,
+      title: 'May with Torchic',
+      description: 'Pokemon Kotobukiya May with Torchic Figurine',
+      price: 2900
+    },
+    {
+      id: 14,
+      image: Six,
+      title: 'May with Torchic',
+      description: 'Pokemon Kotobukiya May with Torchic Figurine',
+      price: 2900
     }
-  ];
+  ]);
 
   const showMoreProducts = () => {
     setVisibleProducts(products.length);
   };
+
+  const gifRef = useRef(null);
+
   useEffect(() => {
     const interval = setInterval(() => {
       if (gifRef.current) {
@@ -172,18 +216,11 @@ const Home = () => {
     return () => clearInterval(interval);
   }, []);
 
-
-
-  //TIMERRRR
-
   return (
     <div className='home-container'>
-
-
       <div className="moving-text">
         <p>This project is not finished yet.</p>
       </div>
-
 
       <div className='slider-container'>
         <Slider {...settings}>
@@ -205,14 +242,14 @@ const Home = () => {
         </Slider>
       </div>
 
-
       <div className='timer-bg'>
         <CountdownTimer />
       </div>
+
       <div className="home-container">
         <h1>Featured Products</h1>
         <div className="product-list">
-          {products.map(product => (
+          {products.slice(0, visibleProducts).map((product) => (
             <Product
               key={product.id}
               image={product.image}
@@ -221,20 +258,25 @@ const Home = () => {
               price={product.price}
             />
           ))}
-
         </div>
-        <div className='view-more'><button className='view-more-btn' onClick={showMoreProducts}>View more</button>
-        </div>
+        {visibleProducts < products.length && (
+          <div className='view-more'>
+            <button className='view-more-btn' onClick={showMoreProducts}>
+              View more
+            </button>
+          </div>
+        )}
       </div>
-
-      {/*       <div> <button onClick={}> View more</button></div> */}
 
       <div className='animes'>
         <img src={Animes} alt="" />
       </div>
+
       <div className='vid-container'>
         <div>
-          <div className='ATC'><h1>The ATC Story</h1></div>
+          <div className='ATC'>
+            <h1>The ATC Story</h1>
+          </div>
           <img
             className='circular-vid'
             ref={gifRef}
@@ -246,49 +288,16 @@ const Home = () => {
 
       <div className='popular-brands-title'>
         <h1>Popular Brands</h1>
-
         <div className='brands-card'>
-
-
-
           <div><img style={{ height: 'auto', width: '100px' }} src={Megahouse} alt="" /></div>
           <div><img src={Kotobukiya} alt="" /></div>
           <div><img style={{ height: 'auto', width: '100px' }} src={Aoshima} alt="" /></div>
           <div><img src={Banpresto} alt="" /></div>
           <div><img style={{ height: 'auto', width: '100px' }} src={Bandai} alt="" /></div>
-          <div><img src={IchibanKuji} alt="" /></div>
-        </div>
-
-      </div>
-
-
-      <div className='map-section'>
-        <h1 className='store-loc'>Store Location</h1>
-        <div className='map-container'>
-          <div className='gmap-frame'>
-            <iframe
-              id='gmap-canvas'
-              width="520"
-              height="400"
-              frameBorder="0"
-              scrolling="no"
-              marginHeight="0"
-              marginWidth="0"
-              src="https://maps.google.com/maps?width=100%25&amp;height=400&amp;hl=en&amp;q=Sta.%20Cruz%20sur,%20Iriga%20City%20Camarines%20sur+(Anino's%20Toy%20Collections)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
-            </iframe>
-          </div>
-          <div className='store-address'>
-            <p><b>Address:</b> Zone 3, Sta. Cruz Sur, Iriga City</p>
-            <p><b>Zip code:</b> 4431</p>
-            <p><b>Landmark:</b> Iriga Cit Hall</p>
-            <p><b>Contact Number:</b> 09947946338</p>
-          </div>
+          <div><img style={{ height: 'auto', width: '100px' }} src={IchibanKuji} alt="" /></div>
         </div>
       </div>
-
-
     </div>
-
   );
 };
 
